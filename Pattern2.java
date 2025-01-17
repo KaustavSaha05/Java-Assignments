903b295db848810fbad494db69e4c245f437c8e0
Kaustav Saha <kaustav.saha04@gmail.com>
2025-01-17 22:46:32 +0530

    sixth commit

---
+++ Pattern2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Pattern2 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = scanner.nextInt();
+        
+        for(int i=1; i<=rows; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+        System.out.print("\n");
+        }
+
+        for(int i=rows-1; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+        System.out.print("\n");
+        }
+        scanner.close();
+    }
+}
